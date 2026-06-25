d908be3f48dcc0ecc8ca31c9b849ed5e161f9bff
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2026-06-25 20:18:17 +0330

    Update routes.no-i18n.ts

--- src/js/routes.no-i18n.ts
+++ src/js/routes.no-i18n.ts
@@ -1,3 +1,17 @@
-export function getRoute(path: string) {
+function normalizePath(path: string) {
+  if (!path.startsWith("/")) path = `/${path}`;
+
+  // ensure trailing slash (Astro: trailingSlash: "always")
+  if (!path.endsWith("/")) path = `${path}/`;
   return path;
+}
+
+export function getRoute(locale: string | undefined, path: string) {
+  const cleanPath = normalizePath(path);
+
+  if (!locale || locale === "en") {
+    return cleanPath;
+  }
+
+  return normalizePath(`/${locale}${cleanPath}`);
 }
